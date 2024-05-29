21d8f9f90b795f77ccd015e68f9cdd7bbf8bd07e
maicol Ascencio <mascencio63@uan.edu.co>
2024-05-29 16:05:04 -0500

    Eliminar carpeta de plantilla

---
+++ app/views/admin/entorno_Gestionar_tienda.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../../../public/css/estilos.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="menu1">
+        <img src="../../../public/images/logos/36ed206f-c52a-467b-84eb-4f164b3f303a-removebg-preview.png" alt="" class="logo5">
+        <ul>
+            <li><a href="entorno_administrador.php">Inicio</a></li>
+            <li><a href="#">Configuración de Cuenta</a></li>
+            <li><a href="../index.php">Cerrar Sesión</a></li>
+        </ul>
+    </nav>
+
+    <h2 class="Gestionar2">Gestionar tienda</h2>
+
+    <form class="form">
+        <p class="title">Registrar tienda</p>
+        <p class="message">Ingrese la tienda que desea Registrar  </p>
+            <div class="flex">
+            <label>
+                <input class="input" type="text" placeholder="" required="">
+                <span>Nombre</span>
+            </label>
+    
+            <label>
+                <input class="input" type="text" placeholder="" required="">
+                <span>Dirreción</span>
+            </label>
+        </div>  
+                
+        <label>
+            <input class="input" type="text" placeholder="" required="">
+            <span>Ubicación</span>
+        </label> 
+            
+        <label>
+            <input class="input" type="email" placeholder="" required="">
+            <span>correo</span>
+        </label>
+        <label>
+            <input class="input" type="number" placeholder="" required="">
+            <span>Teléfono</span>
+        </label>
+        <button class="submit">Registrar</button>
+    
+    </form>
+    <h2 class="Gestionar3">Si deseas consultar o modificar la tienda haz clic aqui</h2>
+
+    <button class="shadow__btn1">clic aqui</button>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    
+<footer>
+  <!-- Footer main -->
+  <section class="ft-main">
+    <div class="ft-main-item">
+      <h2 class="ft-title">About</h2>
+      <ul>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Pricing</a></li>
+        <li><a href="#">Customers</a></li>
+        <li><a href="#">Careers</a></li>
+      </ul>
+    </div>
+    <div class="ft-main-item">
+      <h2 class="ft-title">Resources</h2>
+      <ul>
+        <li><a href="#">Docs</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">eBooks</a></li>
+        <li><a href="#">Webinars</a></li>
+      </ul>
+    </div>
+    <div class="ft-main-item">
+      <h2 class="ft-title">Contact</h2>
+      <ul>
+        <li><a href="#">Help</a></li>
+        <li><a href="#">Sales</a></li>
+        <li><a href="#">Advertise</a></li>
+      </ul>
+    </div>
+    <div class="ft-main-item">
+      <h2 class="ft-title">Stay Updated</h2>
+      <p>Subscribe to our newsletter to get our latest news.</p>
+      <form>
+        <input type="email" name="email" placeholder="Enter email address">
+        <input type="submit" value="Subscribe">
+      </form>
+    </div>
+  </section>
+  
+  <!-- Footer social -->
+  <section class="ft-social">
+    <ul class="ft-social-list">
+      <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+      <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+      <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+      <li><a href="#"><i class="fab fa-github"></i></a></li>
+      <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+      <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+    </ul>
+  </section>
+
+  <!-- Footer legal -->
+  <section class="ft-legal">
+    <ul class="ft-legal-list">
+      <li><a href="#">Terms &amp; Conditions</a></li>
+      <li><a href="#">Privacy Policy</a></li>
+      <li>&copy; 2019 Copyright Nowrap Inc.</li>
+    </ul>
+  </section>
+</footer>
+
+</body>
+</html>
