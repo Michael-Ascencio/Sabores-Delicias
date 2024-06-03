17505630545a4f3377f3102e3267f552a248d737
fuad888 <ffranco14@uan.edu.co>
2024-06-02 22:45:34 -0500

    avance inventario

---
+++ app/Views/inventario/entorno_inventario.php
@@ -0,0 +1,16 @@
+<?php
+function enlace($url)
+{
+    $enlace = 'http://' . $_SERVER['HTTP_HOST'] . $url;
+    echo $enlace;
+}
+?>
+<!-- Carpeta en la cual se encuentra el layout -->
+<?php echo $this->extend('Plantilla/layout_admin'); ?>
+<!-- Nombre del contenido en el layout -->
+<?php echo $this->section('contenido'); ?>
+
+<title><?php echo esc($titulo); ?> </title>
+
+
+<?php echo $this->endSection(); ?>
