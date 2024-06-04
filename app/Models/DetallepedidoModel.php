f2633f025f39d687ca64a43767b64616c11da107
Darkpand1811d <luisalejandro14122002@gmail.com>
2024-06-04 12:23:25 -0500

    actualizacion de modelos

--- app/Models/DetallepedidoModel.php
+++ app/Models/DetallepedidoModel.php
@@ -9,12 +9,12 @@ class ClienteModel extends Model
     protected $table      = 'detallepedido';
     protected $primaryKey = 'id_detalle';
 
-    protected $useAutoIncrement = false;
+    protected $useAutoIncrement = true;
 
     protected $returnType     = 'object';
     protected $useSoftDeletes = false;
 
-    protected $allowedFields =['id_detalle', 'Estado', 'created_by', 'creaded_at', 'updated_by', 'updated_at', 'Pedido_id_pedido', 'Refrigerio_has_Producto_Refrigerio_idRefrigerio1 ','Refrigerio_has_Producto_Producto_id_producto1 '];
+    protected $allowedFields =['id_detalle', 'Estado', 'created_by', 'creaded_at', 'updated_by', 'updated_at', 'Pedido_id_pedido', 'Refrigerio_has_Producto_Refrigerio_idRefrigerio1 ', 'cantidad_de_producto','Refrigerio_has_Producto_Producto_id_producto1 '];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
