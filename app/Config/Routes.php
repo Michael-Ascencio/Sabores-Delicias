c1a264cade231f5c503a1d244d9ccf01994d2636
Santiago <santiago.gutierrezyg@gmail.com>
2024-06-03 20:59:54 -0500

    CRUD de empresa, falta funcionalidad botón de busqueda

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -18,11 +18,25 @@ $routes->get('/admin/tienda/modificar/(:num)', 'Administrador::verTiendaConsulta
 $routes->get('/admin/productos/add', 'AñadirProducto::index');
 $routes->post('/admin/productos/add', 'AñadirProducto::subir');
 $routes->get('/Inventario/entorno_inventario', 'Inventario::gestionarInventario');
+$routes->get('/administrador/entorno_gestionar_cliente', 'Administrador::gestionarCliente');
+$routes->get('/administrador/entorno_gestionar_empresa', 'Administrador::gestionarEmpresa');
+$routes->post('/administrador/entorno_gestionar_empresa', 'Administrador::transaccionEmpresa');
+$routes->get('/administrador/entorno_consulta_empresa', 'Administrador::consultarEmpresa');
+$routes->get('/administrador/entorno_editar_empresa/(:num)', 'Administrador::editarEmpresa/$1');
+$routes->post('/administrador/actualizar', 'Administrador::actualizarDatosBD');
 
 /* Cliente */
 $routes->get('/logincliente', 'Cliente::login');
 $routes->get('/cliente/entorno', 'Cliente::entornoCliente');
 $routes->get('/cliente/consumo', 'Cliente::consumo');
 $routes->get('/cliente/configuracion', 'Cliente::configuracion');
-$routes->get('/Inventario/entorno_inventario', 'Inventario::gestionarInventario');
+
+/* Empleado */
+$routes->get('/loginempleado', 'Empleado::login');
+
+/* Contador */
+$routes->get('/logincontador', 'Contador::login');
+
+
+
 /* $routes->get('/modificartienda', 'Administrador::transaccionTienda'); */
