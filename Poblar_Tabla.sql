72dc590a5c39eb995dec8bba9cc33d2962e600ec
maicol Ascencio <mascencio63@uan.edu.co>
2024-06-03 19:12:01 -0500

    Actualizacion de las tablas e ingreso de datos en tablas - Yesid

--- Poblar_Tabla.sql
+++ Poblar_Tabla.sql
@@ -1,5 +1,5 @@
 -- Poblar la tabla Tienda
-INSERT INTO `sYd`.`Tienda` (`cod_postal`, `nombre`, `dirección`, `ubicacion`, `correo`, `teléfono`, `creaded_by`, `creaded_at`, `updated_by`, `updated_at`)
+INSERT INTO `sYd`.`Tienda` (`cod_postal`, `nombre`, `direccion`, `ubicacion`, `correo`, `telefono`, `creaded_by`, `creaded_at`, `updated_by`, `updated_at`)
 VALUES 
 ('199115', 'Tienda Central', 'Calle Falsa 123', 'Centro', 'central@tienda.com', 1234567890, 'admin', '2023-01-01', 'admin', '2023-01-01');
 
@@ -12,12 +12,18 @@ VALUES
 ('Logística', 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00'),
 ('T.I', 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00');
 
--- Poblar la tabla Cliente
-INSERT INTO `sYd`.`Cliente` (`cedula`, `nombre`, `apellido`, `correo`, `contrasena`, `created_by`, `creaded_at`, `updated_by`, `updated_at`, `Area_id_area`, `telefono`)
+-- Poblar la tabla Empresa
+INSERT INTO `sYd`.`Empresa` (`nit`,`nombre`,`direccion`,`telefono`,`created_by`, `created_at`, `updated_by`, `updated_at`)
 VALUES 
-(54997632, 'Juan', 'Pérez', 'juan.perez@example.com', '54997632', 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 1, 1234567890),
-(1005469824, 'María', 'Gómez', 'maria.gomez@example.com', '1005469824', 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 2, 3288846987),
-(108745412, 'María', 'Gutierrez', 'maria.gutierrez@example.com', '108745412', 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 3, 3135468795);
+(800123456,'SUDESPENSA','Calle 100 #10-20',3137894561,'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00'),
+(900987654,'TIENDAS ARA','Carrera 15 #25-30',3004445621,'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00');
+
+-- Poblar la tabla Cliente
+INSERT INTO `sYd`.`Cliente` (`cedula`, `nombre`, `apellido`, `correo`, `contrasena`, `created_by`, `created_at`, `updated_by`, `updated_at`, `Area_id_area`, `telefono`, `Empresa_nit`)
+VALUES 
+(54997632, 'Juan', 'Pérez', 'juan.perez@example.com', '54997632', 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 1, 1234567890,800123456),
+(1005469824, 'María', 'Gómez', 'maria.gomez@example.com', '1005469824', 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 2, 3288846987,900987654),
+(108745412, 'María', 'Gutierrez', 'maria.gutierrez@example.com', '108745412', 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 3, 3135468795,900987654);
 
 -- Poblar la tabla Cargo
 INSERT INTO `sYd`.`Cargo` (`nombre`, `created_by`, `creaded_at`, `updated_by`, `updated_at`)
@@ -27,15 +33,14 @@ VALUES
 ('Vendedor', 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00');
 
 -- Poblar la tabla Empleado
-INSERT INTO `sYd`.`Empleado` (`Cedula`, `nombre`, `apellido`, `contraseña`, `correo`, `teléfono`, `created_by`, `created_at`, `updated_by`, `updated_at`, `Cargo_id_cargo`)
+INSERT INTO `sYd`.`Empleado` (`cedula`, `nombre`, `apellido`, `contrasena`, `correo`, `telefono`, `created_by`, `created_at`, `updated_by`, `updated_at`, `Cargo_id_cargo`)
 VALUES 
 (12345, 'admin', 'admin', 'password123', 'ana.lopez@example.com', 3135489654, 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 1);
 
-INSERT INTO `sYd`.`Empleado` (`Cedula`, `nombre`, `apellido`, `contraseña`, `correo`, `teléfono`, `created_by`, `created_at`, `updated_by`, `updated_at`, `Cargo_id_cargo`, `Tienda_cod_postal`)
+INSERT INTO `sYd`.`Empleado` (`Cedula`, `nombre`, `apellido`, `contrasena`, `correo`, `telefono`, `created_by`, `created_at`, `updated_by`, `updated_at`, `Cargo_id_cargo`, `Tienda_cod_postal`)
 VALUES 
 (1554786423, 'Ana', 'López', 'password123', 'ana.lopez@example.com', 3135489654, 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 2, 199115),
 (52448302, 'Carlos', 'Martínez', 'password123', 'carlos.martinez@example.com', 3759842157, 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 3, 199115);
-
 
 -- Poblar la tabla Proveedor
 INSERT INTO `sYd`.`Proveedor` (`Nit`, `nombre_empresa`, `direccion`, `correo`, `teléfono`, `created_by`, `creaded_at`, `updated_by`, `updated_at`)
@@ -50,12 +55,12 @@ VALUES
 (200, 'Lote2', '2024-12-31', 'admin', '2023-01-01 00:00:00', '2023-01-01 00:00:00', 'admin');
 
 -- Poblar la tabla Producto
-INSERT INTO `sYd`.`Producto` (`nombre`, `precio`, `imagen`, `unidad_medida`, `estado`, `created_by`, `created_at`, `updated_by`, `updated_at`, `Inventario_id_inventario`)
+INSERT INTO `sYd`.`Producto` (`nombre`, `precio`, `imagen`, `unidad_medida`, `estado`, `created_by`, `created_at`, `updated_by`, `updated_at`, `Inventario_id_inventario`, `descripcion`)
 VALUES 
-('Pasabocas doritos flamin hot', 7560, 'imagen1.jpg', '175 g', 1, 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 1),
-('Manzana Red Delicious Importada', 7845, 'imagen2.jpg', '500 gr', 1, 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 2),
-('Soda Schweppes', 7845, 'imagen3.jpg', '1.5 ml', 1, 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 2),
-('Gaseosa COLOMBIANA Sin Azúcar Botella', 4340, 'imagen4.jpg', '2.5 ml', 1, 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 2);
+('Pasabocas doritos flamin hot', 7560, 'imagen1.jpg', '175 g', 1, 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 1,' Pasabocas doritos flamin hot. Se trata de deliciosos Doritos sabor a chile picante que son perfectos para disfrutar en cualquier momento del día. El empaque contiene 175 gramos de pasabocas crujientes y llenos de sabor.'),
+('Manzana Red Delicious Importada', 7845, 'imagen2.jpg', '500 gr', 1, 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 2, 'La Manzana Red Delicious Importada es una fruta fresca y jugosa, importada de las mejores huertas del mundo. Cada unidad pesa alrededor de 500 gramos y está cuidadosamente seleccionada para garantizar la máxima calidad y frescura.'),
+('Soda Schweppes', 7845, 'imagen3.jpg', '1.5 ml', 1, 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 2, 'La Soda Schweppes es una bebida refrescante y burbujeante que combina perfectamente con cualquier comida. Presentada en una botella de 1.5 litros, esta soda es ideal para saciar la sed en cualquier ocasión.'),
+('Gaseosa COLOMBIANA Sin Azúcar Botella', 4340, 'imagen4.jpg', '2.5 ml', 1, 'admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 2, 'La Gaseosa COLOMBIANA Sin Azúcar Botella es una bebida sin calorías perfecta para aquellos que buscan disfrutar del sabor refrescante de la gaseosa sin preocuparse por el azúcar. Cada botella de 2.5 litros está llena de sabor colombiano auténtico y refrescante.');
 
 -- Poblar la tabla Pedido
 INSERT INTO `sYd`.`Pedido` (`created_by`, `creaded_at`, `updated_by`, `updated_at`, `fecha`, `Estado`, `Cliente_id_cliente`)
@@ -70,7 +75,7 @@ VALUES
 ('admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 2);
 
 -- Poblar la tabla Pedido_proveedor
-INSERT INTO `sYd`.`Pedido_proveedor` (`created_by`, `creaded_at`, `updated_by`, `updated_at`, `Empleado_Cedula`, `Proveedor_Nit`, `Producto_id_producto`)
+INSERT INTO `sYd`.`Pedido_proveedor` (`created_by`, `creaded_at`, `updated_by`, `updated_at`, `Empleado_cedula`, `Proveedor_Nit`, `Producto_id_producto`)
 VALUES 
 ('admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 1554786423, 111222333, 1),
 ('admin', '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 1554786423, 444555666, 2);
