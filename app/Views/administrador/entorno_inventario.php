145e3fda65f9a3c117dac648a15d52ad0569809c
fuad888 <ffranco14@uan.edu.co>
2024-06-05 23:23:08 -0500

    avances crud inventario

--- app/Views/administrador/entorno_inventario.php
+++ app/Views/administrador/entorno_inventario.php
@@ -20,17 +20,7 @@ function enlace($url)
     <div class="alert alert-danger">
         <?= session('error') ?>
     </div>
-<?php endif; ?>
-<div class="searchBox">
-<form id="searchForm" method="get" action="<?= base_url('/administrador/entorno_editar_inventario') ?>">
-    <input class="searchInput" type="text" id="nitInput" placeholder="Search something">
-    <button class="searchButton" type="submit">
-      <svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 29 29" fill="none">
-        <g clip-path="url(#clip0_2_17)">
-          <g filter="url(#filter0_d_2_17)">
-            <path d="M23.7953 23.9182L19.0585 19.1814M19.0585 19.1814C19.8188 18.4211 20.4219 17.5185 20.8333 16.5251C21.2448 15.5318 21.4566 14.4671 21.4566 13.3919C21.4566 12.3167 21.2448 11.252 20.8333 10.2587C20.4219 9.2653 19.8188 8.36271 19.0585 7.60242C18.2982 6.84214 17.3956 6.23905 16.4022 5.82759C15.4089 5.41612 14.3442 5.20435 13.269 5.20435C12.1938 5.20435 11.1291 5.41612 10.1358 5.82759C9.1424 6.23905 8.23981 6.84214 7.47953 7.60242C5.94407 9.13789 5.08145 11.2204 5.08145 13.3919C5.08145 15.5634 5.94407 17.6459 7.47953 19.1814C9.01499 20.7168 11.0975 21.5794 13.269 21.5794C15.4405 21.5794 17.523 20.7168 19.0585 19.1814Z" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" shape-rendering="crispEdges"></path>
-          </g>
-        </g>
+<?php endif; ?>      
         <defs>
           <filter id="filter0_d_2_17" x="-0.418549" y="3.70435" width="29.7139" height="29.7139" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
             <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
@@ -50,11 +40,16 @@ function enlace($url)
     </button>
   </form>
 </div>
+<h3 class="my-3" id="titulo">Inventario</h3>
+        <table class="table table-hover table-bordered my-3" aria-describedby="titulo">
+          <a href="nuevo.html" class="btn btn-success">Agregar</a>
 
-    <table class="table table-striped">
+    <table class="table-dark">
         <thead>
             <tr>
             <th scope="col">id_inventario</th>
+            <th scope="col">id_tienda</th>
+            <th scope="col">id_producto</th>
             <th scope="col">cantidad</th>
             <th scope="col">lote</th>
             <th scope="col">fecha_caducidad</th>
@@ -64,17 +59,24 @@ function enlace($url)
         <tbody>
     <?php foreach ($inventarios as $inventario) : ?>
       
-            <tr>
+        <tr>
             <div class="Tabla_Inventario">
             <td> <?php echo $inventario->id_inventario; ?> </td>
-            <td> <?php echo $inventario->cantidad; ?> </td>
             <td> <?php echo $inventario->lote; ?> </td>
             <td> <?php echo $inventario->fecha_caducidad; ?> </td>
+            <td> <?php echo $inventario->cantidad; ?> </td>
+            <td> <?php echo $inventario->lote; ?> </td>
+            <td> <?php echo $inventario->fecha_caducidad; ?> </td>           
             <td>
-                <a href="<?= base_url('/administrador/editar_inventario/' . $inventario->id_inventario) ?>" class="btn btn-primary">Editar</a>
+                <a href="edita.html" class="btn btn-warning btn-sm me-2">Editar</a>
+                <a href="edita.html" class="btn btn-warning btn-sm me-2">Eliminar</a>
             </td>
-            </tr>
+          </div>
+        </tr>
     <?php endforeach; ?>
         </tbody>
     </table>
 <?php echo $this->endSection(); ?>
+                        
+
+
---
+++ app/Views/administrador/entorno_registro_inventario.php
@@ -0,0 +1,19 @@
+<?php
+function enlace($url)
+{
+    $enlace = 'http://' . $_SERVER['HTTP_HOST'] . $url;
+    echo $enlace;
+}
+?>
+
+<!-- Carpeta en la cual se encuentra el layout -->
+<?php echo $this->extend('Plantilla/layout_login'); ?>
+<!-- Nombre del contenido en el layout -->
+<?php echo $this->section('contenido');?>
+
+
+<div class="imagen12">
+<img src="<?php enlace('/Sabores-Delicias/public/images/logos/36ed206f-c52a-467b-84eb-4f164b3f303a-removebg-preview.png');?>" alt="" class="logo2">
+</div>
+
+<?php echo $this->endSection(); ?>
